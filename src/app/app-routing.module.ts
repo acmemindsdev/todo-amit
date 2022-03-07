c8eca70c3b394eda8f3dca8ed1ee8c58bbf58bfc
prodeep-acme <82568274+prodeep-acme@users.noreply.github.com>
2022-03-07 12:21:53 +0530

    Revert "add side navbar and add some functionality"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
 const routes: Routes = [
-  {
-    path: '', redirectTo: 'list', pathMatch: 'full'
-  },
-  // {
-  //   path: 'tasks', loadChildren: () => import('./list/tasks/tasks.module').then(m => m.TasksModule)
-  // },
-  {
-    path: 'list', loadChildren: () => import('./list/list.module').then(m => m.ListModule)
-  }
+  { path:'', redirectTo:'tasks', pathMatch:'full' },
+  { path:'tasks',component:TasksComponent }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,71 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule } from '@angular/material/grid-list';
-
+import {MatButtonModule} from '@angular/material/button'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input'
+import {MatRadioModule} from '@angular/material/radio'
+import {MatSelectModule} from '@angular/material/select'
+import {MatSliderModule} from '@angular/material/slider'
+import {MatAutocompleteModule} from '@angular/material/autocomplete'
+import { MatDialogModule } from "@angular/material/dialog";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {TasksComponent} from './pages/tasks/tasks.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { HeaderComponent } from './pages/header/header.component';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { ContentComponent } from './pages/content/content.component';
+import { AddTaskComponent } from './pages/content/add-task/add-task.component';
+import { TaskComponent } from './pages/content/task/task.component';
+import { AllTaskListComponent } from './pages/content/task/all-task-list/all-task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TasksComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent,
+    AddTaskComponent,
+    TaskComponent,
+    AllTaskListComponent
   ],
   imports: [
-    CommonModule,
+    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatAutocompleteModule,
+    MatDialogModule,
+    NgbModule,
+    MatGridListModule,
+    MatCardModule,
+    FormsModule,
+    MatDividerModule,
+    MatListModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+
   ],
-  exports: [MatGridListModule],
+  exports:[
+    MatGridListModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [],
+  schemas: [  ]
 })
-export class AppModule {}
+export class AppModule { }
