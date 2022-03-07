850e12cf6e899a7aa4d20af2daf3da9d198ea116
amitAMPL <amit@acmeminds.com>
2022-03-07 18:18:42 +0530

    new folder structure

--- src/app/list/list-routing.module.ts
+++ src/app/list/list-routing.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './list.component';
+import { TaskComponent } from './content/task/task.component';
 
 const routes: Routes = [
     {
       path:'', component: ListComponent
     },
+    // {
+    //   path: 'testlist1', component: ListComponent
+    // },
+    // {
+    //   path: 'testlist2', component: ListComponent,
+    // },
     {
-      path: 'testlist1', component: ListComponent
+      path: 'task', component: TaskComponent
     },
     {
-      path: 'testlist2', component: ListComponent,
-    },
+      // path: 'task', loadChildren: () => import('./content/task/task.component').then(m => m.TaskComponent)
+    }
 ];
 
 @NgModule({
