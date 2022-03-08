3e236c964ecf7449df5f3e1e0394a3e017624036
amitAMPL <amit@acmeminds.com>
2022-03-08 19:51:39 +0530

    on route change show the tasks list in main content

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -6,21 +6,11 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { FormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { TaskComponent } from './content/task/task.component';
 import { AllTaskListComponent } from './content/task/all-task-list/all-task-list.component';
@@ -40,21 +30,11 @@ import { AddTaskComponent } from './content/add-task/add-task.component';
     CommonModule,
     ListRoutingModule,
     MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatSliderModule,
-    MatAutocompleteModule,
-    NgbModule,
     MatGridListModule,
-    MatCardModule,
     FormsModule,
     MatDividerModule,
-    MatListModule,
     MatExpansionModule,
     MatCheckboxModule,
-    MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
   ]
